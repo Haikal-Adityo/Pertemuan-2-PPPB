e81455dbc4376261b77773232b9c4afb8326a1ea
Haikal-Adityo <haikaladityo@gmail.com>
2023-08-24 18:42:41 +0700

    commit pertemuan 2

--- app/src/main/java/com/example/pertemuan1/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.pertemuan1
 
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var number: Float = 0.0F
+    private var number = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,26 +55,62 @@ class MainActivity : AppCompatActivity() {
             }
             false
         }
+
+        // Membuat hanya bisa input angka
+        binding.BeratBadan.inputType = EditorInfo.TYPE_CLASS_NUMBER
+        binding.TinggiBadan.inputType = EditorInfo.TYPE_CLASS_NUMBER
+
+        binding.TinggiBadan.setOnKeyListener { _, keyCode, _ ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                return@setOnKeyListener true
+            }
+            false
+        }
     }
 
     private fun hitungBMI() {
         val berat = binding.BeratBadan.text.toString().toFloat()
         val tinggi = binding.TinggiBadan.text.toString().toFloat() / 100
-        val bmi = berat / (tinggi * tinggi)
 
-        val roundedBmi = String.format("%.1f", bmi)
+        if (berat <= 0 || tinggi <= 0) {
+            Toast.makeText(this@MainActivity, "Please enter valid values.", Toast.LENGTH_SHORT).show()
+            return
+        }
 
-        number = roundedBmi.toFloat()
-        binding.txtNumber.text = roundedBmi
+        val bmi = (berat / (tinggi * tinggi)).toInt()
 
-        // Mengubah warna text sesuai nilai BMI
+        number = bmi
+        binding.txtNumber.text = number.toString()
+
+        // Mengubah warna text dan menampilkan message sesuai nilai BMI
         when {
-            bmi < 17 -> binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorSevere))
-            bmi >= 17 && bmi < 18.5 -> binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorLow))
-            bmi >= 18.5 && bmi < 25 -> binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorNormal))
-            bmi >= 25 && bmi < 30 -> binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorLow))
-            else -> binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorSevere))
+            bmi < 17 -> {
+                binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorSevere))
+                showToast("Low BMI")
+            }
+            bmi >= 17 && bmi < 18.5 -> {
+                binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorLow))
+                showToast("Moderate BMI")
+            }
+            bmi >= 18.5 && bmi < 25 -> {
+                binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorNormal))
+                showToast("Normal BMI")
+            }
+            bmi in 25..29 -> {
+                binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorLow))
+                showToast("Slightly high BMI")
+            }
+            else -> {
+                binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorSevere))
+                showToast("Very high BMI")
+            }
         }
     }
 
+
+    private fun showToast(message: String) {
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+    }
+
 }
+
