beca3db55f6af56587cd1a02188946528dcf9ee9
Haikal-Adityo <haikaladityo@gmail.com>
2023-08-24 13:47:30 +0700

    commit pertemuan 2

--- app/src/main/java/com/example/pertemuan1/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan1/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.pertemuan1
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.pertemuan1.databinding.ActivityMainBinding
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var number = 0
+    private var number: Float = 0.0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,17 +30,50 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnHitung.setOnClickListener {
-            calculateAndDisplayBMI()
+            val berat = binding.BeratBadan.text.toString()
+            val tinggi = binding.TinggiBadan.text.toString()
+
+            if (berat.isNotEmpty() && tinggi.isNotEmpty()) {
+                hitungBMI()
+            } else {
+                Toast.makeText(this@MainActivity, "Please fill in all fields.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Mengunci key Enter
+        binding.BeratBadan.setOnKeyListener { _, keyCode, _ ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                return@setOnKeyListener true
+            }
+            false
+        }
+
+        binding.TinggiBadan.setOnKeyListener { _, keyCode, _ ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                return@setOnKeyListener true
+            }
+            false
         }
     }
 
-    private fun calculateAndDisplayBMI() {
+    private fun hitungBMI() {
         val berat = binding.BeratBadan.text.toString().toFloat()
         val tinggi = binding.TinggiBadan.text.toString().toFloat() / 100
         val bmi = berat / (tinggi * tinggi)
 
         val roundedBmi = String.format("%.1f", bmi)
 
+        number = roundedBmi.toFloat()
         binding.txtNumber.text = roundedBmi
+
+        // Mengubah warna text sesuai nilai BMI
+        when {
+            bmi < 17 -> binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorSevere))
+            bmi >= 17 && bmi < 18.5 -> binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorLow))
+            bmi >= 18.5 && bmi < 25 -> binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorNormal))
+            bmi >= 25 && bmi < 30 -> binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorLow))
+            else -> binding.txtNumber.setTextColor(ContextCompat.getColor(this, R.color.colorSevere))
+        }
     }
+
 }
